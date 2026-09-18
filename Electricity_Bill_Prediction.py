1a2caab0f5f7bc4535e8b1d23df2ac7ae4575315
AbhayAR <143073396+4bh4y4r@users.noreply.github.com>
2026-09-18 09:11:54 +0530

    Update Electricity_Bill_Prediction.py

--- Electricity_Bill_Prediction.py
+++ Electricity_Bill_Prediction.py
@@ -6,7 +6,7 @@ import joblib
 from pathlib import Path
 
 
-MODEL_PATH = Path(__file__).resolve().parent / "polynomial_regression_electric_bill.pkl"
+MODEL_PATH = Path(__file__).resolve().parent / "polynomial_regression_electric_bill (1).pkl"
 
 
 @st.cache_resource
